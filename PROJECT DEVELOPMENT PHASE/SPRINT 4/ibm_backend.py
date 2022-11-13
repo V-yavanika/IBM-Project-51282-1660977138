3470404fdfdf668abf39b28e6839d4105d016bd0
akkaash <58299603+akkaash123@users.noreply.github.com>
2022-11-13 20:11:14 +0530

    Update ibm_backend.py

--- PROJECT DEVELOPMENT PHASE/SPRINT 4/ibm_backend.py
+++ PROJECT DEVELOPMENT PHASE/SPRINT 4/ibm_backend.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import requests
 
 # NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
-API_KEY = "_7Z1sbsvzYS3HGHmSdfCxJjMzY-yAzk_85tkhdWVcCqb"
+API_KEY = " "
 token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
 mltoken = token_response.json()["access_token"]
 header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
